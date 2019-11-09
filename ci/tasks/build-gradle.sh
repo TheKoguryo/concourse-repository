f770ec85719eb9aefd1044ddbf8a4d7bf0bd757e
DongHee Lee <TheKoguryo@gmail.com>
2019-11-09 20:38:47 +0900

    Create build-gradle.sh

---
+++ ci/tasks/build-gradle.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e -x
+
+ls -la $GRADLE_HOME
+ls -la $GRADLE_HOME/bin
+
+cd source-repo
+$GRADLE_HOME/bin/gradle --version
+$GRADLE_HOME/bin/gradle --build-cache clean assemble
+
+ls -la build/libs/
+
+cd ..
+
+cp source-repo/build/libs/*.jar build-output/
