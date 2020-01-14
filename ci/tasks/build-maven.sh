41eb45b51ed7b96d75f36bab2f977dff1ac813ec
DongHee Lee <TheKoguryo@gmail.com>
2020-01-15 04:30:13 +0900

    Update build-maven.sh

--- ci/tasks/build-maven.sh
+++ ci/tasks/build-maven.sh
@@ -4,7 +4,7 @@ set -e -x
 
 mkdir -p $MAVEN_CONFIG
 
-cat > $MAVEN_CONFIG/settings.xml.xxx <<EOF
+cat > $MAVEN_CONFIG/settings.xml <<EOF
 <?xml version="1.0" encoding="UTF-8"?>
 <settings xmlns="http://maven.apache.org/SETTINGS/1.1.0"
   xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
@@ -13,7 +13,7 @@ cat > $MAVEN_CONFIG/settings.xml.xxx <<EOF
     <mirror>
       <id>central</id>
       <name>central</name>
-      <url>http://10.193.193.200:8081/nexus/content/repositories/thirdparty/</url>
+      <url>http://10.193.193.26:8081/nexus/content/repositories/thirdparty/</url>
       <mirrorOf>*</mirrorOf>
     </mirror>
   </mirrors>
